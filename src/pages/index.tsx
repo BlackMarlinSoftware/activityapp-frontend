13d6c184834fe40d7d3fa0d193cae985e01d21ac
Joe-Brady <joebradydev@gmail.com>
2022-06-28 14:46:39 +0100

    Add descriptions

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,6 +17,7 @@ const Home: NextPage = () => (
         <div key={activity.id}>
           <h2>{activity.name}</h2>
           <p>Categories: {activity.categories?.map(({ name }) => (name ? CategoryName[name] : '')).join(', ')}</p>
+          <p>Description: {activity.description}</p>
           <p>Location: {activity.location?.name}</p>
           <p>Run by: {activity.host?.name}</p>
           <p>
