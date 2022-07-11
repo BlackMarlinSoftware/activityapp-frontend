0c8454411647a14087e9fc5a7b17029673b75b94
Guilherme Reis <contactmewk@gmail.com>
2022-07-11 11:34:26 +0100

    [JMG] new homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,8 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import { brixtonData } from '../brixtonData';
-import ActivityCard from '../components/activityCard';
 import styled from 'styled-components';
-
-// Throwaway container, just didn't want to break the test layout
-// as I have removed a nonsense global width.
-export const ActivityContainer = styled.div`
-  background-color: white;
-  width: 700px;
-  max-height: 100%;
-`;
+import Link from 'next/link';
+import {LOCATIONS} from '../types';
 
 const Home: NextPage = () => (
   <div>
@@ -21,12 +13,13 @@ const Home: NextPage = () => (
     </Head>
 
     <main>
-      <ActivityContainer>
-        <h1>Activities</h1>
-        {brixtonData.activities?.map((activity) => (
-          <ActivityCard activity={activity} key={activity.id} />
-        ))}
-      </ActivityContainer>
+      <h1>Locations</h1>
+      {Object.values(LOCATIONS).map(location => (
+        <div key={location}>
+          <Link href={`/location/${location}`}><a>{location}</a></Link>
+        </div>
+      ))
+      }
     </main>
   </div>
 );
