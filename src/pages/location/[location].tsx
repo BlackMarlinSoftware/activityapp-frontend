9619972429dfe691b844e888a6e64cd08bb2fca7
Guilherme Reis <contactmewk@gmail.com>
2022-07-11 17:49:59 +0100

    [JMG] Remove styles with pages dir

--- src/pages/location/[location].tsx
+++ src/pages/location/[location].tsx
@@ -2,9 +2,31 @@ import {NextPage} from 'next';
 import {useRouter} from 'next/router';
 import Header from '../../components/Header';
 import MapContainer from '../../components/MapContainer';
-import {Container, List, ActivityContainer} from './styles';
 import {dataSelection, initialViewStateSelection, LOCATIONS} from '../../types';
 import ActivityCard from '../../components/ActivityCard';
+import styled from 'styled-components';
+import {spacing} from '../../styles/theme';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  background-color: white;
+  width: 100%;
+  height: calc(100vh - ${spacing['8']});
+`;
+
+const List = styled.div`
+  width: 50%;
+  background-color: white;
+  max-height: 100%;
+  overflow: auto;
+`;
+
+const ActivityContainer = styled.div`
+  background-color: white;
+  width: 100%;
+  max-height: 100%;
+`;
 
 const ListingPage: NextPage = () => {
   const router = useRouter();
