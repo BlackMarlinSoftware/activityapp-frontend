8a3b5fd7bf21cc76657562ac46aedffb535d0557
Joe-Brady <joebradydev@gmail.com>
2022-07-17 16:32:01 +0100

    Swap initial activities page props to SSR

--- src/pages/activities.tsx
+++ src/pages/activities.tsx
@@ -1,5 +1,4 @@
-import { NextPage } from 'next';
-import { useRouter } from 'next/router';
+import { GetServerSideProps, NextPage } from 'next';
 import Header from '../components/Header';
 import client from '../apollo-client';
 import { ACTIVITIES } from '../queries/activities.query';
@@ -7,7 +6,6 @@ import MapContainer from '../components/MapContainer';
 import styled from 'styled-components';
 import { spacing } from '../styles/theme';
 import { ActivitiesQuery } from '../generated/graphql';
-import { ViewState } from 'react-map-gl';
 import ActivityList from '../components/ActivityList';
 import { Activities } from '../types';
 import { currentMapState, MapCoords } from '../reactiveVars/map';
@@ -25,28 +23,18 @@ const Container = styled.div`
 `;
 
 interface Props {
-  activities: Activities;
+  initialActivities?: Activities;
+  initialMapCoords?: MapCoords;
 }
 
-const ActivitiesPage: NextPage<Props> = ({ activities }) => {
-  const router = useRouter();
-  const { latitude, longitude, zoom } = router.query;
-
-  if (typeof latitude === 'string' && typeof longitude === 'string' && typeof zoom === 'string') {
-    const initialMapCoords: MapCoords = {
-      latitude: Number(latitude),
-      longitude: Number(longitude),
-      zoom: Number(zoom),
-    };
-
-    currentMapState(initialMapCoords);
-
+const ActivitiesPage: NextPage<Props> = ({ initialActivities, initialMapCoords }) => {
+  if (initialActivities && initialMapCoords) {
     return (
       <PageContainer>
         <Header />
         <Container>
-          <ActivityList activities={activities} />
-          <MapContainer activities={activities} initialViewState={initialMapCoords} />
+          <ActivityList activities={initialActivities} />
+          <MapContainer activities={initialActivities} initialViewState={initialMapCoords} />
         </Container>
       </PageContainer>
     );
@@ -62,15 +50,30 @@ const ActivitiesPage: NextPage<Props> = ({ activities }) => {
 
 export default ActivitiesPage;
 
-export async function getServerSideProps(): Promise<{ props: Props }> {
-  const { data } = await client.query<ActivitiesQuery>({
-    query: ACTIVITIES,
-    fetchPolicy: 'no-cache',
-  });
+export const getServerSideProps: GetServerSideProps = async (context): Promise<{ props: Props }> => {
+  const { latitude, longitude, zoom } = context.query;
 
-  return {
-    props: {
-      activities: data.activities,
-    },
-  };
-}
+  if (typeof latitude === 'string' && typeof longitude === 'string' && typeof zoom === 'string') {
+    const initialMapCoords: MapCoords = {
+      latitude: Number(latitude),
+      longitude: Number(longitude),
+      zoom: Number(zoom),
+    };
+
+    currentMapState(initialMapCoords);
+
+    const { data } = await client.query<ActivitiesQuery>({
+      query: ACTIVITIES,
+      fetchPolicy: 'no-cache',
+    });
+
+    return {
+      props: {
+        initialActivities: data.activities,
+        initialMapCoords: initialMapCoords,
+      },
+    };
+  }
+
+  return { props: {} };
+};
