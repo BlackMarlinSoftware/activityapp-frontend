b2db9b564f94a779a6eef5594499fa1fe86acf2c
Joe-Brady <joebradydev@gmail.com>
2022-07-17 16:11:50 +0100

    Update query params on map move

--- src/components/MapContainer/index.tsx
+++ src/components/MapContainer/index.tsx
@@ -1,9 +1,10 @@
 import Map, { Marker } from 'react-map-gl';
 import { ViewState } from 'react-map-gl/dist/esm/types/external';
 import ActivityCardMap from '../ActivityCardMap';
-import { currentFocusedActivityId, currentMapState } from '../../reactiveVars/map';
+import { currentFocusedActivityId, currentMapState, MapCoords } from '../../reactiveVars/map';
 import { useReactiveVar } from '@apollo/client';
 import { Activities, Activity } from '../../types';
+import { useRouter } from 'next/router';
 
 interface Props {
   activities: Activities;
@@ -12,18 +13,25 @@ interface Props {
 
 const MapContainer = ({ activities, initialViewState }: Props): JSX.Element => {
   const focusedActivityId = useReactiveVar(currentFocusedActivityId);
+  const router = useRouter();
+
+  const updateMapQuery = ({ latitude, longitude, zoom }: MapCoords) => {
+    router.replace({ query: { ...router.query, latitude, longitude, zoom } });
+  };
 
   return (
     <>
       {initialViewState && (
         <Map
           initialViewState={initialViewState}
-          onMove={(evt) => {
-            currentMapState({
+          onMoveEnd={(evt) => {
+            const newCoords: MapCoords = {
               longitude: evt.viewState.longitude,
               latitude: evt.viewState.latitude,
               zoom: evt.viewState.zoom,
-            });
+            };
+            currentMapState(newCoords);
+            updateMapQuery(newCoords);
           }}
           style={{ width: '100%', height: '100%' }}
           mapStyle="mapbox://styles/mapbox/streets-v9"
