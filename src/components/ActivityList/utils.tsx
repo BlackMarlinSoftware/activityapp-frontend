8ceec30c9fb4daa510e6b687e8ce07fc5b9d3aa4
Merrickfox <merrick.fox@gmail.com>
2022-07-29 13:41:34 +0100

    remove loading util, keep file as we might need it?

--- src/components/ActivityList/utils.tsx
+++
@@ -1,6 +0,0 @@
-import ActivityCardLoading from '../ActivityCardLoading';
-
-export const loadingShimmerActivities = (count: number) =>
-  Array.from({ length: count === 0 ? 10 : count }, (_, index) => {
-    return <ActivityCardLoading key={index} />;
-  });
