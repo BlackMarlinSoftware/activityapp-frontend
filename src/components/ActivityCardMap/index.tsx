a09fd778b53b0e3015811ea68adba0c36d111007
Joe-Brady <joebradydev@gmail.com>
2022-09-17 23:13:57 +0100

    Add basics of multi activity locations

--- src/components/ActivityCardMap/index.tsx
+++ src/components/ActivityCardMap/index.tsx
@@ -20,65 +20,91 @@ import Link from 'next/link';
 import { activityTypeLabelMap, capitalFirstLetter } from '../../utils/activity.utils';
 import useBase64Url from '../../hooks/useBase64Url';
 
-interface Props {
+interface PopoverProps {
   activity: ActivityListing;
-  focused: boolean;
+  close: () => void;
 }
 
-const ActivityCardMap = ({ activity, focused }: Props): JSX.Element => {
-  const categoryName = activity.activities_x_categories[0].category.name;
-
-  const [isOpen, setIsOpen] = useState(false);
+const ActivityPopover = ({ activity, close }: PopoverProps) => {
   const currentUrlBase64 = useBase64Url();
   const activityTypeLabel = activityTypeLabelMap[activity.type];
+
+  return (
+    <Link href={`/activity/${activity.id}?backUrl=${currentUrlBase64}`} passHref>
+      <PopoverCard>
+        <ImageContainer>
+          {/* <FavoriteContainer>
+        <FavoriteOutline />
+      </FavoriteContainer> */}
+
+          <CloseContainer
+            onClick={(event) => {
+              event.stopPropagation();
+              close();
+            }}
+          >
+            <Icon icon="X" colour="hsl(0,0%,90%)" width="12px" height="12px" />
+          </CloseContainer>
+
+          {activity.activities_x_media.length > 0 ? (
+            <Image
+              src={`/images/${activity.id}/${activity.activities_x_media[0].media.path}`}
+              alt={activity.activities_x_media[0].media.caption || 'Activity image'}
+              style={{ borderTopLeftRadius: cardBorderRadius, borderTopRightRadius: cardBorderRadius }}
+              objectFit="cover"
+              objectPosition="center"
+              width={imageContainerWidth}
+              height={imageContainerHeight}
+            />
+          ) : null}
+        </ImageContainer>
+        <DetailsContainer>
+          <h5>{activity.name}</h5>
+          <h6>
+            {capitalFirstLetter(activityTypeLabel)} run by {activity.host.name}
+          </h6>
+        </DetailsContainer>
+      </PopoverCard>
+    </Link>
+  );
+};
+
+interface Props {
+  activities: ActivityListing[];
+  focused: boolean;
+  locationId: string;
+}
+
+const ActivityCardMap = ({ activities, focused, locationId }: Props): JSX.Element => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedActivityIndex, setSelectedActivityIndex] = useState(0);
+
+  const firstActivityCategoryName = activities[0].activities_x_categories[0].category.name;
 
   return (
     <ActivityContainer>
       <Popover
         placement="top"
-        locationId={activity.location.id}
+        locationId={locationId}
         isOpenSetter={setIsOpen}
-        render={({ close, labelId, descriptionId }) => (
-          <Link href={`/activity/${activity.id}?backUrl=${currentUrlBase64}`} passHref>
-            <PopoverCard>
-              <ImageContainer>
-                {/* <FavoriteContainer>
-                  <FavoriteOutline />
-                </FavoriteContainer> */}
-
-                <CloseContainer
-                  onClick={(event) => {
-                    event.stopPropagation();
-                    close();
-                  }}
-                >
-                  <Icon icon="X" colour="hsl(0,0%,90%)" width="12px" height="12px" />
-                </CloseContainer>
-
-                {activity.activities_x_media.length > 0 ? (
-                  <Image
-                    src={`/images/${activity.id}/${activity.activities_x_media[0].media.path}`}
-                    alt={activity.activities_x_media[0].media.caption || 'Activity image'}
-                    style={{ borderTopLeftRadius: cardBorderRadius, borderTopRightRadius: cardBorderRadius }}
-                    objectFit="cover"
-                    objectPosition="center"
-                    width={imageContainerWidth}
-                    height={imageContainerHeight}
-                  />
-                ) : null}
-              </ImageContainer>
-              <DetailsContainer>
-                <h5>{activity.name}</h5>
-                <h6>
-                  {capitalFirstLetter(activityTypeLabel)} run by {activity.host.name}
-                </h6>
-              </DetailsContainer>
-            </PopoverCard>
-          </Link>
+        render={({ close }) => (
+          <div>
+            <ActivityPopover close={close} activity={activities[selectedActivityIndex]} />
+            {activities.length > 1 && (
+              <div>
+                <h4 onClick={() => setSelectedActivityIndex((current) => current - 1)}>Previous</h4>
+                <h4 onClick={() => setSelectedActivityIndex((current) => current + 1)}>Next</h4>
+              </div>
+            )}
+          </div>
         )}
       >
         <Pin open={isOpen || focused}>
-          <Icon icon={categoryName} colour={isOpen ? 'white' : 'black'} />
+          {activities.length === 1 ? (
+            <Icon icon={firstActivityCategoryName} colour={isOpen ? 'white' : 'black'} />
+          ) : (
+            <h4>{activities.length}</h4>
+          )}
         </Pin>
       </Popover>
     </ActivityContainer>
